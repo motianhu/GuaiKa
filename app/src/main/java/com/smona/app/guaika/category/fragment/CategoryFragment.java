1e38d0bce23757b78af9e6d76b5dc5f6602f61fd
moth <moth@gionee.com>
2018-07-23 17:52:01 +0800

    分类优化

--- app/src/main/java/com/smona/app/guaika/category/fragment/CategoryFragment.java
+++ app/src/main/java/com/smona/app/guaika/category/fragment/CategoryFragment.java
@@ -2,10 +2,9 @@ package com.smona.app.guaika.category.fragment;
 
 import com.smona.app.guaika.R;
 import com.smona.app.guaika.category.presenter.CategoryPresenter;
-import com.smona.app.guaika.home.fragment.VideoFragment;
 import com.smona.app.guaika.home.presenter.VideoPresenter;
 
-public class CategoryFragment extends VideoFragment {
+public class CategoryFragment extends AbsCagetoryFragment {
     @Override
     public VideoPresenter createPresenter() {
         return new CategoryPresenter();
